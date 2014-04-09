89b92bcaefbf46d4b1e4abcdfad3baff4c62338c
Brian McInnis <brian.j.mcinnis@gmail.com>
2014-04-09 09:17:38 -0400

    Updates
    Updates

--- 20140225 R - Intelligence Squared US.R
+++ 20140225 R - Intelligence Squared US.R
@@ -1,18 +1,29 @@
 ## Intelligence Squared US (I2US)
-## 2014-02-25 Brian McInnis
+## 2014-02-25 Brian McInnis (Last update: 2014-04-04)
 ## Objective:  Depict turntaking during debate section (R3)
  #  by LIWC function word categories (as percent of statement words)
  #  also showing Opening Statement and Closing Statements as
  #  baseline.
 
+## Note on the commenting scheme:
+ #  In most cases the questions/steps were written before any of the
+ #  R script, and for that reason there's an odd commenting scheme.
+ #  The first large blocks of code are devoted to building up the
+ #  local dataset so that we can do some work on it.  The remainder
+ #  of the script is commented with the following question-answer
+ #  pattern.  You will see blocks of questions:  (1) Q1, (2) Q2, etc.
+ #  followed by blocks of steps in answering the questions: [1.a], 
+ #  [1.b], for question 1 and [2.a], [2.b] for question 2.
+
 ## Clear memory
-#   Quite likely you will need to run this program twice,
-#   Clearing the memory each time to build both the DR and DD
-#   Objects.  This is mainly because the full text is included.
+ #  Quite likely you will need to run this program twice,
+ #  Clearing the memory each time to build both the DR and DD
+ #  Objects.  This is mainly because the full text is included.
+
 rm(list = ls());
 
 ## Install and load any necessary packages
-pack <- c('plyr','xlsx','XLConnect','ggplot2','data.table','reshape','tm', 'topicmodels');
+pack <- c('plyr','doBy','xlsx','XLConnect','ggplot2','data.table','reshape','tm', 'topicmodels');
 for (p in pack) {
   print(p);
   if(p %in% rownames(installed.packages()) == FALSE){
@@ -75,11 +86,49 @@ if (exists("dd",where=i2us)==FALSE){
 
 attach(i2us);
 
-## Create an outcome variable to indicate which side won the debate
+## Explore the dataset a little bit:
+ # (1) Does one side generally win the debate?
+ # (2) What is the average margin of victory?
+ # (3) Does the side that talks more win?
 
-dr$outcome[(dr$poll.delta.against>dr$poll.delta.for)]<-"against";
-dr$outcome[(dr$poll.delta.against<dr$poll.delta.for)]<-"for";
-dr$outcome[(dr$outcome=="")]<-"undecided";
+ ## [1.a:  Create outcome and margin variables to indicate which side won the debate]
+  dr$outcome[(dr$poll.delta.against>dr$poll.delta.for)]<-1;
+  dr$outcome[(dr$poll.delta.against<dr$poll.delta.for)]<-(-1);
+  dr$outcome[(dr$outcome==NA)]<-0;
+  dr$outcome<-as.numeric(dr$outcome);
+  
+  dr$persuadable<-(dr$poll.before.undecided-dr$poll.after.undecided);
+  dr$margin.abs<-(dr$poll.after.for-dr$poll.after.against);
+  dr$margin.delta<-(dr$poll.delta.for-dr$poll.delta.against);
+
+ ## [1.b:  Tabulate by outcome and then by margin]
+  addmargins(xtabs(formula=~title+outcome, data=dr),1);
+  # (21 files) 11/10 (52.3% AGAINST vs. FOR)
+  summaryBy(formula=poll.before.undecided~outcome, data=dr, FUN=c(mean,median));
+  # (21 files) A little less than a THIRD of the audience is undecided ahead of a debate
+  summaryBy(formula=poll.after.undecided~outcome, data=dr, FUN=c(mean,median));
+  # (21 files) After the debate about 8% remain undecided, meaning that on avg. 22% of the audience is persuadable
+  
+  summaryBy(formula=persuadable~outcome, data=dr, FUN=c(mean,median));
+  # (21 files) 22-24% on avg. are persuaded through the debate, assuming that the
+  #   Audience members with existing opinions do not change their vote.
+
+  summaryBy(formula=poll.delta.against~outcome, data=dr, FUN=c(mean,median));
+  summaryBy(formula=poll.delta.for~outcome, data=dr, FUN=c(mean,median));
+  # (21 files) Though we don't know whether there is back and forth within the
+  #   populations with existing opinions, neither side appears to lose support
+  #   regardless of who wins a debate.
+ 
+  summaryBy(formula=margin.abs~outcome, data=dr, FUN=c(mean,median));
+  # (21 files) The margin of victory (by audience final polling) is on avg. 34-35%
+  summaryBy(formula=margin.delta~outcome, data=dr, FUN=c(mean,median));
+  # (21 files) The margin of victory (by delta) is between 14 and 22%
+
+  cor.test(formula=~outcome+poll.before.for,data=dr);
+  cor.test(formula=~outcome+poll.before.against,data=dr);
+  cor.test(formula=~outcome+poll.before.undecided,data=dr);
+  cor.test(formula=~outcome+persuadable,data=dr);
+
 
 ## Convert LIWC scores to percentages
 # (1) Identify all of the LIWC variables of interest
@@ -114,7 +163,8 @@ eLIWC <- function(x){
   for (i in 1:nrow(x)) {
     for (t in liwc){
       #print(paste('---',t,sep=""));
-      x[i,paste("perc.",t,sep="")] <-(sum(x[seq(from=1,to=i),t])/sum(x[seq(from=1,to=i),"liwcScores.total.words"]))*100;
+      #ROLLING AVERAGE x[i,paste("perc.",t,sep="")] <-(sum(x[seq(from=1,to=i),t])/sum(x[seq(from=1,to=i),"liwcScores.total.words"]))*100;
+      x[,paste("perc.",t,sep="")] <-(x[,t]/x[,"liwcScores.total.words"])*100;
     }
   }
   return(x);
@@ -159,25 +209,36 @@ eLSM <- function(x){
 }
 dd.lsm<-ddply(dd.lsm,.(title,section,speakingOrder), eLSM);
 
+
+eNumber <- function(x){
+  for (i in 1:nrow(x)){
+    x[i,"bin"]<-floor(i/5);    
+    x[i,"line.bin"]<-i;    
+  }
+  return(x);
+}
+dd.lsm<-ddply(dd.lsm,.(title,section,role), eNumber);
+
+
 ## Create several GGPLOTs to depict the LSM by utterance during the debate section
 for (t in titles){
   print(t);
-  tsub <- subset(dd.lsm,(title==t)&((speakingOrder=="against<-for")|(speakingOrder=="for<-against")|(speakingOrder=="for<-moderator")|(speakingOrder=="against<-moderator"))&((section=="R3")));
+  #tsub <- subset(dd.lsm,(title==t)&((speakingOrder=="against<-for")|(speakingOrder=="for<-against")|(speakingOrder=="for<-moderator")|(speakingOrder=="against<-moderator"))&((section=="R3")));
+  tsub <- subset(dd.lsm,(title==t)&((speakingOrder=="against<-for")|(speakingOrder=="for<-against"))&((section=="R3")));
   lSect <- aggregate(.~section,
                      data=tsub[,c("line","section")],
                      FUN=min)
   p <- ggplot(data=tsub,
               aes(x=line, 
-                  colour=factor(speakingOrder),
-                  y=lsm, 
-                  group=factor(speakingOrder)))+
+                  colour=factor(role),
+                  y=lsm),
+                  group=factor(bin))+
     labs(title = paste("I2US Debate: ",t,sep=""), x = "By utterance (within Debate Section)", y = "Linguistic Style Matching (LSM)")+
     geom_point()+
-    geom_line();
-#    geom_line(aes(x=lSect[(lSect$section=="R3"),"line"], colour="R3"))+ 
-#    geom_line(aes(x=lSect[(lSect$section=="R4"),"line"], colour="R4"))+
-#    stat_smooth(method = "lm");
-  ggsave(plot=p, filename=paste(t,".png",sep=""));
+    geom_line()+
+    stat_smooth(method = "lm");
+
+  ggsave(plot=p, filename=paste("images/",t,".png",sep=""));
   print(p);
 }
 
@@ -186,11 +247,15 @@ for (t in titles){
 #   Provides access to LDA by David Blei: http://cran.r-project.org/web/packages/topicmodels/topicmodels.pdf
 #   (1) Create the term document matrix based on the statement data
 
-#dd$rNames = paste(dd$file, dd$line, sep="_");
-#dd.statement <- data.frame(docs=dd[,"statement"],row.names = c(dd[,"rNames"]));
-#dd.statement <- DataframeSource(x=dd.statement);
+dd$rNames = paste(dd$file, dd$line, sep="_");
+dd.statement <- data.frame(docs=dd[,"statement"],row.names = c(dd[,"rNames"]));
+dd.statement <- DataframeSource(x=dd.statement);
 #inspect(Corpus(dd.statement))
-#dd.tm <- DocumentTermMatrix(Corpus(dd.statement),control = list(removePunctuation = TRUE));
+dd.tm <- DocumentTermMatrix(Corpus(dd.statement),control = list(removePunctuation = TRUE,
+                                                                tolower = TRUE));
+## Add meta data values to the TDM
+meta(dd.tm, '');
+
 #findFreqTerms(dd.tm, 5);
 #findAssocs(dd.tm, "war", 0.5);
 #rowTotals <- data.frame(apply(dd.tm , 1, sum));
